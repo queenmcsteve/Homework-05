7bcef033bf3b5c0fb5455c2f27c96bae4f93bbfc
Queen McSteve <niko.desilva@esmt.org>
2021-10-22 08:58:02 +0200

    added other time-blocks, added current time from moment.js

--- assets/script/script.js
+++ assets/script/script.js
@@ -5,26 +5,14 @@ var currentTime = moment();
 //display date in jumbotron
 $("#currentDay").text(currentTime.format("MMMM Do, YYYY"));
 //create a variable to hold the current hour
-// var currentHour = currentTime.hour();
+var currentHour = currentTime.hour();
 
-//temp currentHour var for building
-var currentHour = 10;
-
-// console.log(currentHour);
-// //translate current time into a time state (past, present, future)
-// function getCurrentTimeState(hour, currentHour) {
-//   return hour < currentHour
-//     ? "past"
-//     : hour === currentHour
-//     ? "present"
-//     : "future";
-// }
-// getCurrentTimeState();
-// console.log(getCurrentTimeState());
+// //temp currentHour var for building
+// var currentHour = 10;
 
 //color blocks based on relative time state
 var colorTimeBlocks = function () {
-  for (var i = 9; i < 12; i++) {
+  for (var i = 9; i < 18; i++) {
     var taskArea = $("#task-" + i);
     if (currentHour > i) {
       $(taskArea).addClass("past");
@@ -39,7 +27,7 @@ colorTimeBlocks();
 
 //add text area to time blocks
 $(".taskBin").on("click", "p", function () {
-  console.log("p element was clicked");
+  // console.log("p element was clicked");
   var text = $(this).text().trim();
   var textInput = $("<textarea>").addClass("form-control").val(text);
 
@@ -50,7 +38,7 @@ $(".taskBin").on("click", "p", function () {
 //update time block with entered text
 $(".taskBin").on("blur", "textarea", function () {
   var text = $(this).val().trim();
-  console.log(text);
+  // console.log(text);
 
   var taskP = $("<p>").addClass("taskItem").text(text);
 
@@ -59,7 +47,7 @@ $(".taskBin").on("blur", "textarea", function () {
 
 //save text entry to localStorage
 $(".saveBtn").on("click", function () {
-  console.log($(".saveBtn").index(this) + "th save button clicked");
+  // console.log($(".saveBtn").index(this) + "th save button clicked");
   var index = $(".saveBtn").index(this);
   tasks[index] = $(this).parent().find(".taskItem").text();
   localStorage.setItem("tasks", JSON.stringify(tasks));
@@ -71,7 +59,7 @@ var loadTasks = function () {
   if (tasks === null) {
     tasks = {};
   }
-  console.log(tasks);
+  // console.log(tasks);
   printTasks();
 };
 
@@ -82,7 +70,7 @@ var printTasks = function () {
       .addClass("description task-item-" + list)
       .text(arr);
 
-    console.log(list + " " + taskP);
+    // console.log(list + " & " + JSON.stringify(tasks));
 
     $("#task-item-" + list).replaceWith(taskP);
   });
